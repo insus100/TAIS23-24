18f09a516b952210a6fdd66ec02030d19c6bd8ca
insus100 <inigovelasco@gmail.com>
2023-10-24 21:37:12 +0200

    cosa

--- TAISProblems/Peaje_a_la_sombra/Peaje_a_la_sombra.cpp
+++ TAISProblems/Peaje_a_la_sombra/Peaje_a_la_sombra.cpp
@@ -10,7 +10,7 @@
 #include <vector>
 #include <queue>
 using namespace std;
-
+using Camino = deque<int>;
 #include "Grafo.h"  // propios o los de las estructuras de datos de clase
 
 /*@ <answer>
@@ -28,7 +28,7 @@ using namespace std;
  //@ <answer>
 class Problema {
 public:
-    Problema(Grafo const& g, int s) : visit(g.V(), false), ant(g.V()), dist(g.V()), s(s) {
+    Problema(Grafo const& g, int s, int trabajo) : visit(g.V(), false), ant(g.V()), dist(g.V()), s(s), trabajo(trabajo) {
         bfs(g);
     }
 
@@ -38,11 +38,19 @@ public:
     int distancia(int v) const {
         return dist[v];
     }
+    Camino camino(int v) const {
+        if (!hayCamino(v)) throw std::domain_error("No existe camino");
+        Camino cam;
+        for (int x = v; x != s; x = ant[x])
+            cam.push_front(x);
+        cam.push_front(s);
+        return cam;
+    }
 private:
     std::vector<bool> visit; // visit[v] = ¿hay camino de s a v?
     std::vector<int> ant; // ant[v] = último vértice antes de llegar a v
     std::vector<int> dist; // dist[v] = aristas en el camino s-v más corto
-    int s;
+    int s, trabajo;
     void bfs(Grafo const& g) {
         std::queue<int> q;
         dist[s] = 0; visit[s] = true;
@@ -73,11 +81,16 @@ bool resuelveCaso() {
         g.ponArista(v1 - 1, v2 - 1);
     }
     // resolver el caso posiblemente llamando a otras funciones
-    Problema alex(g, casaA - 1);
-    Problema tr(g, trabajo - 1);
-    cout << alex.distancia(casaL - 1) << "\n";
+    Problema alex(g, casaA - 1, trabajo - 1);
+    Problema lucas(g, casaL - 1, trabajo - 1);
+    //Problema tr(g, trabajo - 1);
+    /*if (alex.hayCamino(casaL - 1)) {
+        Camino c = alex.camino(casaL - 1);
+        cout << c.front();
+    }*/
+    
     // escribir la solución
-
+    cout << alex.distancia(casaL - 1) + lucas.distancia(trabajo - 1) << "\n";
     return true;
 }
 
